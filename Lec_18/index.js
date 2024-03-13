5cd9759cfc92b9722818d520b911625e21042e26
hafiz <gondal.hafizbilal@gmail.com>
2024-03-13 16:16:34 +0500

    lecture 18

---
+++ Lec_18/index.js
@@ -0,0 +1,45 @@
+// function x() {
+//   console.log("JS");
+// }
+
+// function y(x) {
+//   x();
+// }
+
+const radius = [3, 1, 2, 4];
+
+const area = function (radius) {
+  return Math.PI * radius * radius;
+};
+
+const circumference = function (radius) {
+  return 2 * Math.PI * radius;
+};
+
+const diameter = function (radius) {
+  return 2 * radius;
+};
+
+// const calculate = function (radius, logic) {
+//   const output = [];
+//   for (let i = 0; i < radius.length; i++) {
+//     output.push(logic(radius[i]));
+//   }
+//   return output;
+// };
+
+Array.prototype.calculate = function (logic) {
+  const output = [];
+  for (let i = 0; i < this.length; i++) {
+    output.push(logic(this[i]));
+  }
+  return output;
+};
+
+// console.log(calculate(radius, area));
+// console.log(calculate(radius, circumference));
+// console.log(calculate(radius, diameter));
+
+console.log(radius.calculate(area));
+console.log(radius.calculate(circumference));
+console.log(radius.calculate(diameter));
